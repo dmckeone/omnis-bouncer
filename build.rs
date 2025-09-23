5609bbaceb8b56748cd348df5b8d10b8548ed8c1
David McKeone <david@artsman.com>
2025-09-23 10:43:52 -0600

    Builds with pnpm work properly now

--- build.rs
+++ build.rs
@@ -1,15 +1,56 @@
-// build.rs
+use build_deps::rerun_if_changed_paths;
+use std::path::Path;
+use std::process::Command;
 
-// All queries in the release package go to the server they came from
-static API_URI: &str = "/";
+// Cargo doesn't output normal println!() so use this small macro to print limited output
+macro_rules! log {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", format!($($tokens)*))
+    }
+}
 
 fn main() {
+    log!("Building UI: pnpm run build");
+
+    let ui_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("ui");
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .env("VITE_API_URI", "/")
+            .current_dir(&ui_dir)
+            .args(["/C", "pnpm run build"])
+            .output()
+            .expect("failed to build user interface with pnpm")
+    } else {
+        Command::new("sh")
+            .env("VITE_API_URI", "/")
+            .current_dir(&ui_dir)
+            .arg("-c")
+            .arg("pnpm run build")
+            .output()
+            .expect("failed to build user interface with pnpm")
+    };
+    if !output.status.success() {
+        log!("pnpm failed - exit code: {:?}", output.status.code());
+        if !output.stdout.is_empty() {
+            log!(
+                "pnpm output: {:?}",
+                String::from_utf8(output.stdout).unwrap()
+            );
+        }
+        if !output.stderr.is_empty() {
+            log!(
+                "pnpm error: {:?}",
+                String::from_utf8(output.stderr).unwrap()
+            );
+        }
+    }
+
     // rebuild if build.rs is changed
-    build_deps::rerun_if_changed_paths("build.rs").unwrap();
-    build_deps::rerun_if_changed_paths("ui/package.json").unwrap();
-    build_deps::rerun_if_changed_paths("ui/pnpm-lock.yaml").unwrap();
-    build_deps::rerun_if_changed_paths("ui/vite.config.ts").unwrap();
-    build_deps::rerun_if_changed_paths("ui/dist/").unwrap();
-    build_deps::rerun_if_changed_paths("ui/src/").unwrap();
-    build_deps::rerun_if_changed_paths("ui/src/**").unwrap();
+    rerun_if_changed_paths("build.rs").unwrap();
+    rerun_if_changed_paths("ui/package.json").unwrap();
+    rerun_if_changed_paths("ui/pnpm-lock.yaml").unwrap();
+    rerun_if_changed_paths("ui/vite.config.ts").unwrap();
+    rerun_if_changed_paths("ui/dist").unwrap();
+    rerun_if_changed_paths("ui/src").unwrap();
+    rerun_if_changed_paths("ui/src/**").unwrap();
 }
