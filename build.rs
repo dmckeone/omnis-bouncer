0be7cf1d1116f8aa802adbbe99f4fd5748cd1cd4
David McKeone <david@artsman.com>
2025-09-23 09:52:48 -0600

    Switch to pnpm (which required removing npm_rs)

--- build.rs
+++ build.rs
@@ -1,24 +1,13 @@
 // build.rs
 
-use npm_rs::{NodeEnv, NpmEnv};
-
 // All queries in the release package go to the server they came from
 static API_URI: &str = "/";
 
 fn main() {
-    let _exit_status = NpmEnv::default()
-        .with_node_env(&NodeEnv::from_cargo_profile().unwrap_or_default())
-        .with_env("VITE_API_URI", API_URI) // Ensure that all API requests got to the root to avoid CORS errors
-        .set_path("ui")
-        .init_env()
-        .install(None)
-        .run("build")
-        .exec()
-        .unwrap();
-
     // rebuild if build.rs is changed
     build_deps::rerun_if_changed_paths("build.rs").unwrap();
     build_deps::rerun_if_changed_paths("ui/package.json").unwrap();
+    build_deps::rerun_if_changed_paths("ui/pnpm-lock.yaml").unwrap();
     build_deps::rerun_if_changed_paths("ui/vite.config.ts").unwrap();
     build_deps::rerun_if_changed_paths("ui/dist/").unwrap();
     build_deps::rerun_if_changed_paths("ui/src/").unwrap();
