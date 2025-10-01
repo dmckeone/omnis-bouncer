fed7b82fc586b0a3f5b8508a7350dbb9b3e60d06
David McKeone <david@artsman.com>
2025-10-01 12:27:48 -0600

    Add link to API docs

--- ui/src/components/TopNav.vue
+++ ui/src/components/TopNav.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import { API_URI } from '@/constants.ts'
+
 const props = defineProps<{
   title?: string
 }>()
+
+const docsURI = API_URI + 'docs'
 </script>
 
 <template>
@@ -9,6 +13,11 @@ const props = defineProps<{
     <div class="navbar bg-base-100 shadow-sm">
       <div class="flex-1">
         <a v-if="props.title != undefined" class="btn btn-ghost text-xl">{{ props.title }}</a>
+      </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li><a :href="docsURI">API Docs</a></li>
+        </ul>
       </div>
     </div>
     <div class="flex fixed w-screen">
